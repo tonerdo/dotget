e76870c97269d15d9a31a10d27d6a41b780af3e1
Oluwatoni Solarin-Sodara <oluwatoni.sodara@ithaka.org>
2017-04-21 09:59:36 +0100

    Create preliminary cli app

--- src/DotGet.Cli/Program.cs
+++ src/DotGet.Cli/Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Microsoft.Extensions.CommandLineUtils;
 
 namespace DotGet.Cli
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var app = new CommandLineApplication();
+            app.Name = "dotget";
+            app.FullName = "DotGet Global Package Installer";
+            app.Description = "Install and use command line tools built on .NET Core";
+            app.HelpOption("-h|--help");
+            app.VersionOption("-v|--version", "1.0.0");
+
+            if (args.Length == 0)
+                app.ShowHelp();
+
+            return app.Execute(args);
         }
     }
 }
