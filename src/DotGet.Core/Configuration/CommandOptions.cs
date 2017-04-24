3dd3e800d6d68817bb76e943a826153b3aaa8421
Oluwatoni Solarin-Sodara <oluwatoni.sodara@ithaka.org>
2017-04-24 11:14:29 +0100

    Add contructors to configuration option classes

--- src/DotGet.Core/Configuration/CommandOptions.cs
+++ src/DotGet.Core/Configuration/CommandOptions.cs
@@ -2,5 +2,13 @@ using System.Collections.Generic;
 
 namespace DotGet.Core.Configuration
 {
-    public class CommandOptions : Dictionary<string, string> { }
+    public class CommandOptions : Dictionary<string, string>
+    {
+        public CommandOptions() { }
+        public CommandOptions(Dictionary<string, string> options)
+        {
+            foreach (KeyValuePair<string, string> pair in options)
+                this.Add(pair.Key, pair.Value);
+        }
+    }
 }
--- src/DotGet.Core/Configuration/ResolverOptions.cs
+++ src/DotGet.Core/Configuration/ResolverOptions.cs
@@ -2,5 +2,13 @@ using System.Collections.Generic;
 
 namespace DotGet.Core.Configuration
 {
-    internal class ResolverOptions : Dictionary<string, string> { }
+    internal class ResolverOptions : Dictionary<string, string>
+    {
+        public ResolverOptions() { }
+        public ResolverOptions(Dictionary<string, string> options)
+        {
+            foreach (KeyValuePair<string, string> pair in options)
+                this.Add(pair.Key, pair.Value);
+        }
+    }
 }
