5152f0e36d3b6b5e59aba5a5afcc13f3c4f5ef19
Toni Solarin-Sodara <toni.edward@outlook.com>
2018-01-02 17:57:37 -0700

    modify logger interface and implementation

--- src/DotGet.Cli/Logging/Logger.cs
+++ src/DotGet.Cli/Logging/Logger.cs
@@ -7,56 +7,44 @@ namespace DotGet.Cli.Logging
     {
         public LogLevel Level { get; set; } = LogLevel.Error | LogLevel.Info | LogLevel.Success | LogLevel.Warning;
 
-        public void LogError(string data)
+        public void LogError(string message)
         {
             if (Level.HasFlag(LogLevel.Error))
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(data);
+                Console.WriteLine(message);
                 Console.ResetColor();
             }
         }
 
-        public void LogInformation(string data)
+        public void LogInformation(string message)
         {
             if (Level.HasFlag(LogLevel.Info))
-                Console.WriteLine(data);
+                Console.WriteLine(message);
         }
 
-        public void LogProgress(string data)
-        {
-            if (Level.HasFlag(LogLevel.Info))
-                Console.Write($"{data}... ");
-        }
-
-        public void LogResult(string data)
-        {
-            if (Level.HasFlag(LogLevel.Info))
-                Console.WriteLine($"{data}.");
-        }
-
-        public void LogSuccess(string data)
+        public void LogSuccess(string message)
         {
             if (Level.HasFlag(LogLevel.Success))
             {
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(data);
+                Console.WriteLine(message);
                 Console.ResetColor();
             }
         }
 
-        public void LogVerbose(string data)
+        public void LogVerbose(string message)
         {
             if (Level.HasFlag(LogLevel.Verbose))
-                Console.WriteLine(data);
+                Console.WriteLine(message);
         }
 
-        public void LogWarning(string data)
+        public void LogWarning(string message)
         {
             if (Level.HasFlag(LogLevel.Warning))
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine(data);
+                Console.WriteLine(message);
                 Console.ResetColor();
             }
         }
--- src/DotGet.Core/Logging/ILogger.cs
+++ src/DotGet.Core/Logging/ILogger.cs
@@ -3,12 +3,10 @@ namespace DotGet.Core.Logging
     public interface ILogger
     {
         LogLevel Level { get; set; }
-        void LogSuccess(string data);
-        void LogVerbose(string data);
-        void LogProgress(string data);
-        void LogResult(string data);
-        void LogInformation(string data);
-        void LogWarning(string data);
-        void LogError(string data);
+        void LogSuccess(string message);
+        void LogVerbose(string message);
+        void LogInformation(string message);
+        void LogWarning(string message);
+        void LogError(string message);
     }
 }
